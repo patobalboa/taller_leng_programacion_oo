bfbb7c935140ccbce62ddf639a5dc7af3037d1dd
Patricio Balboa <pbalboa08@gmail.com>
2026-05-12 19:17:11 -0400

    se agrega caso 1 parcial

--- my_project/usuarios/views.py
+++ my_project/usuarios/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import render
+from .db import ConexionDB
 
 # Create your views here.
 def login_view(request):
+    db = ConexionDB()
+
+    sql = "SELECT * FROM usuarios WHERE email = %s AND password = %s AND id_estado = 1"
+    sql_tipo_usuario = "SELECT id_tipo_usuario FROM usuarios WHERE email = %s"
+   
     if request.method == 'POST':
+
         # Aquí puedes agregar la lógica para autenticar al usuario
-        username = request.POST.get('username')
+        email = request.POST.get('email')
         password = request.POST.get('password')
 
-        print(f"Intento de login con username: {username} y password: {password}")
+        login_estado = db.verificar(sql, [email, password])
+
+        if login_estado:
+
+            tipo_usuario = db.consultar(sql_tipo_usuario, [email])
+
+            if tipo_usuario[0]['id_tipo_usuario'] == 1:
+                return render(request, 'portal_vendedor.html')
+            
+            
+            elif tipo_usuario[0]['id_tipo_usuario'] == 2:
+                return render(request, 'portal_supervisor.html')
+        else:
+            print("Login fallido")
+        
         # Por ahora, simplemente renderizamos el mismo formulario
-    return render(request, 'login.html', {'username': username})
+    return render(request, 'login.html')
 
