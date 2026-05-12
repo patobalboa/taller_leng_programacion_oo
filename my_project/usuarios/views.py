7a77fd313b3c61315e46ddc7765336ebff4d4422
Patricio Balboa <pbalboa08@gmail.com>
2026-05-12 19:42:48 -0400

    cambios caso 1 terminado

--- my_project/usuarios/views.py
+++ my_project/usuarios/views.py
@@ -6,7 +6,14 @@ def login_view(request):
     db = ConexionDB()
 
     sql = "SELECT * FROM usuarios WHERE email = %s AND password = %s AND id_estado = 1"
-    sql_tipo_usuario = "SELECT id_tipo_usuario FROM usuarios WHERE email = %s"
+    sql_tipo_usuario = """
+    SELECT 
+     u.nombre, u.email, tu.nombre as nombre_tipo_usuario, tu.id_tipo_usuario
+    FROM usuarios u
+    INNER JOIN tipo_usuario tu
+    ON u.id_tipo_usuario = tu.id_tipo_usuario
+    WHERE email = %s 
+    """
    
     if request.method == 'POST':
 
@@ -21,9 +28,14 @@ def login_view(request):
             tipo_usuario = db.consultar(sql_tipo_usuario, [email])
 
             if tipo_usuario[0]['id_tipo_usuario'] == 1:
+
+                request.session['email'] = email
+                request.session['nombre'] = tipo_usuario[0]['nombre']
+                request.session['tipo_usuario'] = tipo_usuario[0]['nombre_tipo_usuario']
+
                 return render(request, 'portal_vendedor.html')
             
-            
+
             elif tipo_usuario[0]['id_tipo_usuario'] == 2:
                 return render(request, 'portal_supervisor.html')
         else:
